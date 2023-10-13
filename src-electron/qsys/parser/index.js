b9242569f448d9f191f33bb73462edaefa632c4f
jkjh8 <jkjh82@naver.com>
2023-10-13 15:27:35 +0900

    socket send

--- src-electron/qsys/parser/index.js
+++ src-electron/qsys/parser/index.js
@@ -41,6 +41,7 @@ export default function (deviceId, data) {
           break
       }
     }
+
     // by id
     if (Object.keys(obj).includes('id')) {
       const { id, result } = obj
@@ -48,67 +49,36 @@ export default function (deviceId, data) {
         // 1000 get device status
         case 1000:
           qsysData[deviceId].EngineStatus = result
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({ deviceId, key: 'EngineStatus', value: result })
-          )
+          sendSocket(deviceId, 'EngineStatus', { value: result })
           break
         // 2000 set pa feedback
         case 2000:
           qsysData[deviceId].ZoneStatusConfigure = result
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({
-              deviceId,
-              key: 'ZoneStatusConfigure',
-              value: result
-            })
-          )
+          sendSocket(deviceId, 'ZoneStatusConfigure', { value: result })
           break
         //2001 get pa configure
         case 2001:
           // TODO: add qsysData ???
           qsysData[deviceId].PaConfig = result
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({ deviceId, key: 'PaConfig', value: result })
-          )
+          sendSocket(deviceId, 'PaConfig', { value: result })
           break
         // 2002 page message
         case 2002:
           qsysData[deviceId].PageID = result.PageID
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({
-              key: 'page:message',
-              value: result
-            })
-          )
+          sendSocket(deviceId, 'page:message', result)
           break
         // 2003 live page
         case 2003:
           qsysData[deviceId].PageID = result.PageID
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({
-              key: 'page:live',
-              value: result
-            })
-          )
+          sendSocket(deviceId, 'page:live', result)
           break
         // 2008 page stop
         case 2008:
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({ key: 'page:stop', value: result })
-          )
+          sendSocket(deviceId, 'page:stop', { value: result })
           break
         // 2008 page stop
         case 2009:
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({ key: 'page:cancel', value: result })
-          )
+          sendSocket(deviceId, 'page:cancel', { value: result })
           break
         // 3001 get gain and mute
         case 3001:
@@ -122,50 +92,22 @@ export default function (deviceId, data) {
               qsysData[deviceId].ZoneStatus[channel].mute = control.Value
             }
           }
-          socket.emit(
-            'qsys:data',
-            JSON.stringify({
-              deviceId,
-              key: 'GainAndMute',
-              value: qsysData[deviceId].ZoneStatus
-            })
-          )
+          sendSocket(deviceId, 'GainAndMute', {
+            ZoneStatus: qsysData[deviceId].ZoneStatus
+          })
+          break
       }
     }
   }
   // if rt true return socket to qsys data
   if (rt) {
-    socket.emit(
-      'qsys:data',
-      JSON.stringify({
-        key: 'RtByMethod',
-        deviceId,
-        EngineStatus: qsysData[deviceId].EngineStatus,
-        ZoneStatus: qsysData[deviceId].ZoneStatus
-      })
-    )
+    sendSocket(deviceId, 'RtByMethod', {
+      EngineStatus: qsysData[deviceId].EngineStatus,
+      ZoneStatus: qsysData[deviceId].ZoneStatus
+    })
   }
-  // for (let val of rt) {
-  //   if (val) {
-  //     const data = JSON.parse(val)
-  //     // by method
-  //     if (Object.keys(data).includes('method')) {
-  //       byMethod(deviceId, data.method, data.params)
-  //     }
-  //     // by id
-  //     if (Object.keys(data).includes('id')) {
-  //       byId(deviceId, data)
-  //     }
-  //   }
-  // }
-  // socket.emit(
-  //   'qsys:data',
-  //   JSON.stringify({
-  //     deviceId,
-  //     status: qsys[deviceId].status,
-  //     zones: qsys[deviceId].zones,
-  //     ZoneStatusConfigure: qsys[deviceId].ZoneStatusConfigure
-  //   })
-  // )
-  // getPaGainMute(deviceId, zones)
+}
+
+function sendSocket(deviceId, key, value) {
+  socket.emit('qsys', JSON.stringify({ key: key, deviceId, ...value }))
 }
